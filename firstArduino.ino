ad94b06ed8cb2ad3654801c80f001c5368cc98a3
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-17 18:45:37 +0700

    Refactor: change output into Fade LED

--- firstArduino.ino
+++ firstArduino.ino
@@ -22,16 +22,24 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
 
+int led = 14;
+int brightness = 0;
+int fadeAmount = 5;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(5000);                      // wait for a second
+  analogWrite(led, brightness);
+  
+  brightness += fadeAmount;
+
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+
+  delay(50);
 }
